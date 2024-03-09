ce50d69bdc581a4c6e7af4155b7c46c121e067dd
GitButler <gitbutler@gitbutler.com>
2024-03-09 15:40:39 +0700

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - master (refs/gitbutler/master)
       branch head: e061ad3268a4ce9e6bb34a2cefac7eadb79177f1
       - app/Livewire/Client/Rmkt.php
       - resources/views/livewire/client/profile.blade.php
       - resources/views/livewire/client/rmkt.blade.php
    
    Your previous branch was: refs/heads/master
    
    The sha for that commit was: cc678ab964e4fae1b074e15b1786f0146b0f7d36
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- resources/views/livewire/client/profile.blade.php
+++ resources/views/livewire/client/profile.blade.php
@@ -2,15 +2,19 @@
     
     {{$client->name}}<br>
     first time regis {{$client->created_at}}<br>
-    number of rmkt {{$client->rmkt->count()}}<br>
-    rmkt opt 1 month {{$client->rmkt->whereNotNull('option_2')->count()}}<br>
-    rmkt opt 3 month {{$client->rmkt->whereNotNull('option_3')->count()}}<br>
+    number of rmkt {{$client->rmkt->count()}} times<br>
+    rmkt opt 1 month : {{$client->rmkt->whereNotNull('option_2')->count()}} times<br>
+    rmkt opt 3 month : {{$client->rmkt->whereNotNull('option_3')->count()}} times<br>
+    <div class="max-h-64 overflow-y-scroll">
     @foreach ($client->rmkt as $c)
     <div class="p-2 odd:bg-gray-200 rounded my-2">
     {{$c->vet->vet_name}} : select {{$c->option_3?'3':'1'}} month <br>
-    {{$c->created_at->toDateString()}} : {{$c->updated_at->toDateString()}} <br>
-    diff {{$c->created_at->diffInMinutes($c->updated_at)}} min<br>
-    last update {{$c->updated_at->diffForHumans(now())}}
+    {{-- {{$c->created_at->toDateString()}} :  --}}
+    {{$c->updated_at->toDateString()}} <br>
+    {{$c->active_status}} <br>
+    {{-- diff {{$c->created_at->diffInMinutes($c->updated_at)}} min<br> --}}
+    {{-- last update {{$c->updated_at->diffForHumans(now())}} --}}
     </div>
     @endforeach
+    </div>
 </div>
