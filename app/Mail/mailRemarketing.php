eefd99e0e8b4b29e1c2323f16ab5ffcb5e12bce8
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2024-03-16 14:04:04 +0700

    update email template

--- app/Mail/mailRemarketing.php
+++ app/Mail/mailRemarketing.php
@@ -16,6 +16,7 @@ class mailRemarketing extends Mailable
 
     public $client;
     public $status;
+    public $banner;
 
     /**
      * Create a new message instance.
@@ -29,24 +30,75 @@ class mailRemarketing extends Mailable
         $date = Carbon::create(env('RMKT_FINALDATE'));
         if($date->isCurrentDay()){
             $status= "10 วันสุดท้าย ใกล้หมดเวลาอย่าลืมไปใช้สิทธิ์ โปรแกรม LOVE Solution Cat Plus ที่คลินิกหรือโรงพยาบาลสัตว์ ที่ได้ลงทะเบียนไว้ ";
+
+            $lastSelect=$this->client->option_1??$this->client->option_2??$this->client->option_3;
+            $thisSelect=$lastSelect==1?3:1;
+
+            $this->banner=asset('rmktbanner/banner monthly.jpg');
+                $status=[
+                    'หมายเลข '.$client->phone.'',
+                    'คุณมีสิทธิพิเศษเข้าร่วม',
+                    'ถึงเวลาปกป้องปรสิตสำหรับน้อง '.$this->client->pet_name,
+                    'โปรแกรม '.env('APP_NAME').' คงเหลือ ',
+                    'ดูแลปกป้องปรสิตสําหรับน้อง '.$this->client->pet_name,
+                    'สามารถรับสิทธิ์ได้ที่ '.$this->client->vet->vet_name,
+                ];
         }elseif($rmktClient){
             $rmktClientActive=$this->client->rmkt->where('active_status','activated')->last();
             if($rmktClient==$rmktClientActive){
                 $status="ครบ 1 เดือนแล้ว ถึงเวลาที่คุณต้องปกป้อง พาราไซท์  อย่าลืมปกป้อง";
+
+
+                $this->banner=asset('rmktbanner/banner monthly.jpg');
+                $status=[
+                    'หมายเลข '.$client->phone.'',
+                    'ครบระยะเวลา 1 เดือนแล้ว',
+                    'ถึงเวลาปกป้องปรสิตสำหรับน้อง '.$this->client->pet_name,
+                    'อย่าลืม! ดูแลอย่างต่อเนื่อง',
+                    'กับ '.env('APP_NAME'),
+                    env('APP_TAGLINE'),
+                ];
             }else{
                 $lastSelect=$this->client->option_1??$this->client->option_2??$this->client->option_3;
                 $thisSelect=$lastSelect==1?3:1;
                 $status="คุณยังมีสิทธิ ".$thisSelect." เดือน อย่าลืมไปใช้สิทธิ์ โปรแกรม ".env('APP_NAME')." ที่คลินิกหรือโรงพยาบาลสัตว์ ที่ได้ลงทะเบียนไว้";
                 $status="ครบ 1 เดือนแล้ว ถึงเวลาที่คุณต้องปกป้อง พาราไซท์  อย่าลืมปกป้อง";
+
+
+                $this->banner=asset('rmktbanner/banner monthly.jpg');
+                $status=[
+                    'หมายเลข '.$client->phone.'',
+                    'ครบระยะเวลา 1 เดือนแล้ว',
+                    'ถึงเวลาปกป้องปรสิตสำหรับน้อง '.$this->client->pet_name,
+                    'อย่าลืม! ดูแลอย่างต่อเนื่อง',
+                    'กับ '.env('APP_NAME'),
+                    env('APP_TAGLINE'),
+                ];
             }
         }else{
             if($this->client->option_2 && $this->client->option_3){
                 $status="ครบ 1 เดือนแล้ว ถึงเวลาที่คุณต้องปกป้อง พาราไซท์  อย่าลืมปกป้อง";
-                
+                $this->banner=asset('rmktbanner/banner monthly.jpg');
+                $status=[
+                    'หมายเลข '.$client->phone.'',
+                    'ครบระยะเวลา 1 เดือนแล้ว',
+                    'ถึงเวลาปกป้องปรสิตสำหรับน้อง '.$this->client->pet_name,
+                    'อย่าลืม! ดูแลอย่างต่อเนื่อง',
+                    'กับ '.env('APP_NAME'),
+                    env('APP_TAGLINE'),
+                ];
             }else{
                 $lastSelect=$this->client->option_1??$this->client->option_2??$this->client->option_3;
                 $thisSelect=$lastSelect==1?3:1;
-                $status="คุณยังมีสิทธิ ".$thisSelect." เดือน อย่าลืมไปใช้สิทธิ์ โปรแกรม ".env('APP_NAME')." ที่คลินิกหรือโรงพยาบาลสัตว์ ที่ได้ลงทะเบียนไว้";
+                $this->banner=asset('rmktbanner/banner '.$thisSelect.' month.jpg');
+                $status=[
+                    'หมายเลข '.$client->phone,
+                    'คุณมีสิทธิพิเศษเข้าร่วม',
+                    'โปรแกรม '.env('APP_NAME').' คงเหลือ '.$thisSelect.' เดือน',
+                    'ดูแลปกป้องปรสิตสําหรับน้อง '.$this->client->pet_name,
+                    'สามารถรับสิทธิ์ได้ที่ '.$this->client->vet->vet_name,
+                ];
+                // $status="คุณยังมีสิทธิ ".$thisSelect." เดือน อย่าลืมไปใช้สิทธิ์ โปรแกรม ".env('APP_NAME')." ที่คลินิกหรือโรงพยาบาลสัตว์ ที่ได้ลงทะเบียนไว้";
 
                 // dd('send remider in 7 day last select '.$lastSelect);
             }
